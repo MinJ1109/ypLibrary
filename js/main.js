e7a9f1b72369749c926649f4ce2a034f088828f2
LeeM <lovesong001109@gmail.com>
2024-06-08 21:46:05 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+$( document ).ready(function(){
+    conMargin();
+    $(window).resize(conMargin);
+    navSlide();
+    mainSldie();
+    todaySlide();
+    hotSlide();
+});
+function conMargin(){
+    var height = $('header').outerHeight(true);
+    var targetPoint = $('[class$="Container"]');
+    
+    targetPoint.css('margin-top', height);
+}
+
+function navSlide(){
+    var target = $('#navPanel > li');
+
+    target.hover(function(){
+        $(this).children('ol').slideToggle(250);
+    });
+}
+
+function mainSldie(){
+    var prevBtn = $('.mainSlideBox span .prevBtn');
+    var nextBtn = $('.mainSlideBox span .nextBtn');
+    var target = $('.mainSlideBox ul li');
+    var targetCount = target.length;
+
+    prevBtn.click(function(){
+        var targetIndex = target.index($('.mainSlideBox ul li.active'));
+        var prevIndex = (targetIndex === 0) ? targetCount - 1 : targetIndex - 1;
+
+        target.eq(targetIndex).removeClass('active');
+        target.eq(prevIndex).addClass('active');
+    });
+
+    nextBtn.click(function(){
+        var targetIndex = target.index($('.mainSlideBox ul li.active'));
+        var nextIndex = (targetIndex === targetCount - 1) ? 0 : targetIndex + 1;
+
+        target.eq(targetIndex).removeClass('active');
+        target.eq(nextIndex).addClass('active');
+    });
+
+    setInterval(function(){
+        var targetIndex = target.index($('.mainSlideBox ul li.active'));
+        var setIndex = (targetIndex === targetCount - 1) ? 0 : targetIndex + 1;
+
+        target.eq(targetIndex).removeClass('active');
+        target.eq(setIndex).addClass('active');
+    }, 5000); 
+}
+
+function todaySlide(){
+    var prevBtn = $('.todayBook .controlStyle01 .prevBtn');
+    var nextBtn = $('.todayBook .controlStyle01 .nextBtn');
+    var target = $('.todayBook > div ul li');
+    var targetCount = target.length;
+
+    prevBtn.click(function(){
+        var targetIndex = target.index($('.todayBook > div ul li.active'));
+        var prevIndex = (targetIndex === 0) ? targetCount - 1 : targetIndex - 1;
+
+        target.eq(targetIndex).removeClass('active');
+        target.eq(prevIndex).addClass('active');
+    });
+
+    nextBtn.click(function(){
+        var targetIndex = target.index($('.todayBook > div ul li.active'));
+        var nextIndex = (targetIndex === targetCount - 1) ? 0 : targetIndex + 1;
+
+        target.eq(targetIndex).removeClass('active');
+        target.eq(nextIndex).addClass('active');
+    });
+}
+
+function hotSlide(){
+    var prevBtn = $('.hotBook .controlStyle01 .prevBtn');
+    var nextBtn = $('.hotBook .controlStyle01 .nextBtn');
+    var target = $('.hotBook > div ul li');
+    var targetCount = target.length;
+
+    prevBtn.click(function(){
+        var targetIndex = target.index($('.hotBook > div ul li.active'));
+        var prevIndex = (targetIndex === 0) ? targetCount - 1 : targetIndex - 1;
+
+        target.eq(targetIndex).removeClass('active');
+        target.eq(prevIndex).addClass('active');
+    });
+
+    nextBtn.click(function(){
+        var targetIndex = target.index($('.hotBook > div ul li.active'));
+        var nextIndex = (targetIndex === targetCount - 1) ? 0 : targetIndex + 1;
+
+        target.eq(targetIndex).removeClass('active');
+        target.eq(nextIndex).addClass('active');
+    });
+}
